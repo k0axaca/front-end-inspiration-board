5910f9bd362f595ce4f9492723de8a6150c33b13
kristina <kkraft423@gmail.com>
2022-01-03 14:21:12 -0500

    useState added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import BoardForm from "./components/BoardForm";
 import data from "./data.json";
 
 function App() {
+  const [boardData, setBoardData] = useState(data);
   const onSubmitBoardDataHandler = (enteredBoardData) => {
     console.log(enteredBoardData);
     const axios = require('axios');
@@ -141,7 +142,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>{boardTitle}</h1>
+        <h1>Inspo Board</h1>
       </header>
       <main className="container-fluid input-container">
         <SelectBoard boardData={data.boards} onSelectBoard={updateBoard} />
